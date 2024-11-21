f07a83c510d50fe8f0c9d96f9ca1658bd524c965
yigitsariyar23 <yigitsariyar23@gmail.com>
2024-11-22 02:10:16 +0300

    Board member datas

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -25,10 +25,10 @@ import omer from "./assets/portraits/omer.jpg";
 import orkun from "./assets/portraits/orkun.jpg";
 import ozgur from "./assets/portraits/ozgur.jpg";
 import ramazan from "./assets/portraits/ramazan.jpg";
-import samil from "./assets/portraits/samil.jpg";
+import samil from "./assets/portraits/samil.jpeg";
 import talha from "./assets/portraits/talha.jpg";
 import tugba from "./assets/portraits/tugba.jpg";
-import tugce from "./assets/portraits/tugce.jpg";
+import tugce from "./assets/portraits/tugce.jpeg";
 import yigit from "./assets/portraits/yigit.jpg";
 import yusuf from "./assets/portraits/yusuf.jpg";
 import zumra from "./assets/portraits/zumra.jpg";
@@ -64,5 +64,160 @@ export const members = [
 
         image: dogukan,
         coolImage: dogukan
+    },
+    {
+        url: "yigitsariyar",
+        name: "Yiğit",
+        surname: "SARIYAR",
+        title: {
+            tr: "Yönetim Kurulu Başkan Yardımcısı",
+            en: "Executive Board Member"
+        },
+        social: {
+            insta: "https://www.instagram.com/yigitsariyar23/",
+            linkedin: "https://www.linkedin.com/in/yigit-sariyar/",
+            github: "https://github.com/yigitsariyar23",
+        },
+        school: {
+            tr: "İzmir Yüksek Teknoloji Enstitüsü",
+            en: "İzmir Institute of Technology"
+        },
+        department: {
+            tr: "Bilgisayar Mühendisliği",
+            en: "Computer Engineering"
+        },
+        grade: {
+            tr: "3. Sınıf",
+            en: "3. Grade"
+        },
+        email: ["yigitsariyar@std.iyte.edu.tr", "yigitsariyar23@gmail.com"],
+        phone: "+90 551 712 48 44",
+
+        image: yigit,
+        coolImage: yigit
+    },
+    {
+        url: "akinakdogan",
+        name: "Akın",
+        surname: "AKDOĞAN",
+        title: {
+            tr: "Yönetim Kurulu Başkan Yardımcısı",
+            en: "Executive Board Member"
+        },
+        social: {
+            insta: "https://www.instagram.com/aknakdo/",
+            linkedin: "https://www.linkedin.com/in/ak%C4%B1n-akdo%C4%9Fan-963ba315a/",
+            github: "https://github.com/akinivors",
+        },
+        school: {
+            tr: "İzmir Yüksek Teknoloji Enstitüsü",
+            en: "İzmir Institute of Technology"
+        },
+        department: {
+            tr: "Bilgisayar Mühendisliği",
+            en: "Computer Engineering"
+        },
+        grade: {
+            tr: "3. Sınıf",
+            en: "3. Grade"
+        },
+        email: ["meteakdogan@std.iyte.edu.tr", "akinakdoakakdo@gmail.com"],
+        phone: "+90 507 103 66 32",
+
+        image: akin,
+        coolImage: akin
+    },
+    {
+        url: "hamzauyar",
+        name: "Hamza",
+        surname: "UYAR",
+        title: {
+            tr: "Yönetim Kurulu Başkan Yardımcısı",
+            en: "Executive Board Member"
+        },
+        social: {
+            insta: "https://www.instagram.com/hamzauyar0/",
+            linkedin: "https://www.linkedin.com/in/hamza-uyar-507576218/",
+            github: "https://github.com/HamzaUyar",
+        },
+        school: {
+            tr: "İzmir Yüksek Teknoloji Enstitüsü",
+            en: "İzmir Institute of Technology"
+        },
+        department: {
+            tr: "Bilgisayar Mühendisliği",
+            en: "Computer Engineering"
+        },
+        grade: {
+            tr: "3. Sınıf",
+            en: "3. Grade"
+        },
+        email: ["hamzauyar@std.iyte.edu.tr", "rayuh03@gmail.com"],
+        phone: "+90 542 719 07 13",
+
+        image: hamza,
+        coolImage: hamza
+    },
+    {
+        url: "tugceturkmenler",
+        name: "Tuğçe",
+        surname: "TÜRKMENLER",
+        title: {
+            tr: "Yönetim Kurulu Başkan Yardımcısı",
+            en: "Executive Board Member"
+        },
+        social: {
+            insta: "https://www.instagram.com/turkmenlertugce/",
+            linkedin: "www.linkedin.com/in/tuğçetürkmenler",
+            github: "https://github.com/turkmenlertugce",
+        },
+        school: {
+            tr: "İzmir Yüksek Teknoloji Enstitüsü",
+            en: "İzmir Institute of Technology"
+        },
+        department: {
+            tr: "Bilgisayar Mühendisliği",
+            en: "Computer Engineering"
+        },
+        grade: {
+            tr: "4. Sınıf",
+            en: "4. Grade"
+        },
+        email: ["tugceturkmenler@std.iyte.edu.tr", "turkmenlertugce@gmail.com"],
+        phone: "+90 538 090 69 45",
+
+        image: tugce,
+        coolImage: tugce
+    },
+    {
+        url: "neclaakyol",
+        name: "Necla",
+        surname: "AKYOL",
+        title: {
+            tr: "Yönetim Kurulu Başkan Yardımcısı",
+            en: "Executive Board Member"
+        },
+        social: {
+            insta: "https://www.instagram.com/nneeeccllaaa/",
+            linkedin: "https://www.linkedin.com/in/necla-akyol-b4a81a257",
+            github: "https://github.com/neclaakyol",
+        },
+        school: {
+            tr: "İzmir Yüksek Teknoloji Enstitüsü",
+            en: "İzmir Institute of Technology"
+        },
+        department: {
+            tr: "Bilgisayar Mühendisliği",
+            en: "Computer Engineering"
+        },
+        grade: {
+            tr: "2. Sınıf",
+            en: "2. Grade"
+        },
+        email: ["neclaakyol@std.iyte.edu.tr", "naclaakyol0607@gmail.com"],
+        phone: "+90 537 503 35 06",
+
+        image: necla,
+        coolImage: necla
     },
 ];
